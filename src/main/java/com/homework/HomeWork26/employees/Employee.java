18980583a13bbd79a9abdd857df268f7037e6d32
MikhailAkd <mihailbuspoi@gmail.com>
2022-08-31 13:18:29 +0300

    Домашняя работа 2.6: начало

---
+++ src/main/java/com/homework/HomeWork26/employees/Employee.java
@@ -0,0 +1,40 @@
+package com.homework.HomeWork26.employees;
+
+import java.util.Objects;
+
+public class Employee {
+    private final String lastName;
+    private final String firstName;
+
+    public Employee(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "ФИО: " + firstName + " " + lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(lastName, employee.lastName) && Objects.equals(firstName, employee.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+}
+
